4df36fb09d3a1ec3b7981fe654a481783bb5a449
vegetason <irakozepaulin12@gmail.com>
2025-06-24 08:57:05 +0200

    Setting up the backend

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+const express= require('express');
+const morgan=require('morgan');
+const helmet=require('helmet');
+const cors=require('cors');
+const middlewares=require('./middlewares'); 
+
+const app= express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin:"http://localhost/3002"
+}));
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"Hello World!"
+    })
+});
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
+
+const port=process.env.PORT||3000
+
+app.listen(port,()=>{
+    console.log(`listening at http://localhost:${port}`)
+});
