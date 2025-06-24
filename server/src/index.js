d20adf52ad6bcff779307c9a5d3e11c004f70886
vegetason <irakozepaulin12@gmail.com>
2025-06-24 10:46:04 +0200

    Setting up the database

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,19 @@ const express= require('express');
 const morgan=require('morgan');
 const helmet=require('helmet');
 const cors=require('cors');
-const middlewares=require('./middlewares'); 
+const middlewares=require('./middlewares');
+const mongoose=require('mongoose');
+require('dotenv').config();
+const logs=require('./apis/logs') 
 
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser:true,
+})
 const app= express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin:"http://localhost/3002"
+    origin:process.env.FRONTEND_URL
 }));
 
 app.get('/',(req,res)=>{
@@ -16,6 +22,7 @@ app.get('/',(req,res)=>{
         message:"Hello World!"
     })
 });
+app.use('/api/logs',logs);
 
 app.use(middlewares.notFound);
 
